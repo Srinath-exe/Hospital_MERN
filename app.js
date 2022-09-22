b8ddf973062fddc59595870409996ff150219200
Srinath-exe <1406srinath@gmail.com>
2022-09-22 17:44:51 +0530

    yep

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const dbconfig = require('./config/dbconfig')
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(port,()=>{
+    console.log(`Listensing to port ${port}`);
+});
+
